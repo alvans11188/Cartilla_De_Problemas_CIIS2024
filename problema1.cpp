339adcbf98e57ec859d47f44a1f3e1d945d70ca5
Alvans <achicallag@unjbg.edu.pe>
2025-07-31 22:59:08 -0500

    agregando ejercicio 1

---
+++ problema1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int pruebas,casos;
+	vector<int> juegos;
+	int cantidad,alturaJuego;
+	int resultados,altura;
+	int i;
+	int arreglo[100];
+	cin>>casos;
+	for( i=0;i<casos;i++){
+		cin>>cantidad;
+		cin>>altura;
+		arreglo[i]=0;
+		for(int j=0;j<cantidad;j++){
+			cin>>alturaJuego;
+			if (altura>=alturaJuego){
+				arreglo[i]=arreglo[i]+1;
+			}
+			
+		}
+		cout<<arreglo[i]<<endl;
+	}
+	
+	return 0;
+}
